d6af2d9f52af23964764a51f7bc90d3c6dd70343
Aryan Rathore <aryanrathoreop@gmail.com>
2025-06-14 21:09:15 +0530

    Greedy Algorithm: Money Change

--- Algorithms/week3_greedy_algorithms/1_money_change/change.cpp
+++ Algorithms/week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+using namespace std;
+void get_change(int m) {
+  int tens = m/10;
+  m %= 10;
+  int fives = m/5;
+   m %= 5;
+  int ones = m;
+if (tens > 0) std::cout << tens << " ";
+if (fives > 0) std::cout << fives << " ";
+if (ones > 0) std::cout << ones << "\n";
 
-int get_change(int m) {
-  //write your code here
-  return n;
 }
 
 int main() {
   int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  cin >> m;
+  get_change(m);
 }

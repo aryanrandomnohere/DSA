4346ddf7fc7b5f02ff97a2c423c6b6953fd7cf47
Aryan Rathore <aryanrathoreop@gmail.com>
2025-06-19 23:40:33 +0530

    Implemented 5_number_of_inversions

--- Algorithms/week4_divide_and_conquer/5_number_of_inversions/inversions.cpp
+++ Algorithms/week4_divide_and_conquer/5_number_of_inversions/inversions.cpp
@@ -3,13 +3,40 @@
 
 using std::vector;
 
+long long merge_and_count(vector<int> a, vector<int> b, int left, int mid, int right){
+  int i = left;
+  int k = left;
+  int j = right;
+  long long inversions = 0;
+  while(i<mid && j<right){
+    if(a[i] < a[j]){
+      b[k++] = a[i++];
+    }else {
+      b[k++] = a[j++];
+      inversions += mid -i;
+    }
+  }
+while(i<mid){
+  b[k++] = a[i++];
+  }
+  while(j<right){
+    b[k++] = a[j++]; 
+  }
+   for (size_t i = left; i < right; i++) {
+        a[i] = b[i];
+    }
+  return inversions;
+}
+
+
 long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
   long long number_of_inversions = 0;
   if (right <= left + 1) return number_of_inversions;
   size_t ave = left + (right - left) / 2;
   number_of_inversions += get_number_of_inversions(a, b, left, ave);
   number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
+  number_of_inversions += merge_and_count(a,b,left,ave,right);
+   
   return number_of_inversions;
 }
 
